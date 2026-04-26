eb95ae961285d97fa7d1e588c7d58ac7a011acef
Joshua188-bit <203899141+Joshua188-bit@users.noreply.github.com>
2026-04-25 23:55:29 -0600

    Api worked and also fixed the search button

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 window.addEventListener("DOMContentLoaded", loadPopularMovies);
 
 async function loadPopularMovies() {
+  const container = document.getElementById("popular-movies");
+
   try {
     const response = await fetch(
       `${API_URL}/movie/popular?api_key=${API_KEY}`
     );
 
     const data = await response.json();
+
+    data.results.forEach(movie => {
+      const { title, poster_path, vote_average, overview } = movie;
+      container.innerHTML += `
+      div class= <"movie-card">
+      <img src = "https://image.tmdb.org/t/p/w500${poster_path}">
+      <h3>${title}</h3>
+      <p>${overview}</p>
+      <p>${vote_average}</p>
+      </div>`
+    });
   } catch (error) {
     console.error(error);
   }
