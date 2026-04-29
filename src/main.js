f2a3fe3efa8804ff6a3b3b6c27b48e54e981c541
Joshua188-bit <203899141+Joshua188-bit@users.noreply.github.com>
2026-04-29 00:05:14 -0600

    Added a load more to homepage

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+let currentPage = 1;
 window.addEventListener("DOMContentLoaded", loadPopularMovies);
 
 async function loadPopularMovies() {
@@ -5,25 +6,32 @@ async function loadPopularMovies() {
 
   try {
     const response = await fetch(
-      `${API_URL}/movie/popular?api_key=${API_KEY}`
+      `${API_URL}/movie/popular?api_key=${API_KEY}&page=${currentPage}`
     );
 
     const data = await response.json();
 
     data.results.forEach(movie => {
       const { title, poster_path, vote_average, overview, release_date } = movie;
-      container.innerHTML += `
+      const card = document.createElement('div');
+      card.className = 'movie-card';
+      card.innerHTML += `
       <div class= "movie-card">
       <img class="poster" src = "https://image.tmdb.org/t/p/w500${poster_path}">
       <div class = "hover-card">
       <h3>${title}</h3>
-      <p>${overview}</p>
       <p>${vote_average}</p>
       <p>${release_date}</p>
        </div>
       </div>`
+      container.appendChild(card);
     });
   } catch (error) {
     console.error(error);
   }
+}
+
+function loadMore() {
+  currentPage++;
+  loadPopularMovies();
 }
